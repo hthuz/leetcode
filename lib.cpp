720651d2394687574a7beec00c833d87a68cd5b6
Autentico <hanggang.20@intl.zju.edu.cn>
2023-01-22 23:22:32 +0800

    update list function

--- lib.cpp
+++ lib.cpp
@@ -26,3 +26,45 @@ void print_2dvec(vector<vector<int>>& nums)
     }
     cout << "]" << endl;
 }
+
+ListNode* get_list_node(ListNode* head, int index)
+{
+    ListNode* node = head;
+    while(index)
+    {
+        node = node->next;
+        index--;
+    }
+    return node;
+}
+
+ListNode* arr_to_list(int* array, int size)
+{
+    ListNode* head;
+    ListNode* cur;
+    int i;
+    
+    cur = new ListNode(array[0]);
+    head = cur;
+    for(i = 1; i < size ; i++)
+    {
+        ListNode* next = new ListNode(array[i]);
+        cur->next = next;
+        cur = next;
+    }
+    return head;
+}
+
+void print_list(ListNode* head)
+{
+    ListNode* node;
+    node = head;
+    int i = 0;
+    while(node && i < 20)
+    {
+        cout << node->val << " ";
+        node = node->next;
+        i++;
+    }
+    cout << endl;
+}
--- lib.h
+++ lib.h
@@ -5,5 +5,26 @@
 using namespace std;
 
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
 void print_vec(vector<int> nums);
+
 void print_2dvec(vector<vector<int>>& nums);
+
+// Given a singly linked list, get the ith node of the list
+ListNode* get_list_node(ListNode* head, int index);
+
+// Given an array, transform it to a singly linked list
+ListNode* arr_to_list(int* array, int size);
+
+// Given a singly linked list, print all its values
+void print_list(ListNode* head);
+
+
